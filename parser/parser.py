d8b647fb710faf5722f014768f5169717e435814
Hardik Sharma <hardiksharmah73@gmail.com>
2023-10-13 15:26:41 +0530

    added pdfParser

---
+++ parser/parser.py
@@ -0,0 +1,21 @@
+import sys
+sys.path.append('..')
+
+from PyPDF2 import PdfReader
+
+def Parser( pdfFile = '../papers/SampleResearchPaper.pdf'):
+    # First task is to convert the pdf file to a text file.
+    reader = PdfReader(pdfFile)
+    content = ""
+    
+    for page in reader.pages:
+        content += page.extract_text()
+    
+    with open(pdfFile.replace('.pdf', '.txt'), 'w') as f:
+        f.write(content)
+
+    return content
+
+
+if __name__ == "__main__":
+    Parser()
