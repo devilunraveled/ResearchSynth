cad122ec9d068e0aaf8a035c1fbf0a23df0b0c02
ashmitchamoli <ashmitchamoli@outlook.com>
2023-11-13 20:40:34 +0530

    Improved MatchSummarizer class and included more hyperparams

---
+++ scorer.py
@@ -0,0 +1,12 @@
+from rouge_score import rouge_scorer
+
+class Score:
+    def __init__(self, trueSummary = None, predSummary = None):
+        self.trueSummary = trueSummary if trueSummary is not None else ""
+        self.predSummary = predSummary if predSummary is not None else ""
+
+    def rougeScore(self):
+        scorer = rouge_scorer.RougeScorer(['rouge1', 'rouge2', 'rouge3'], use_stemmer=True)
+        scores = scorer.score(self.trueSummary, self.predSummary)
+
+        return scores
